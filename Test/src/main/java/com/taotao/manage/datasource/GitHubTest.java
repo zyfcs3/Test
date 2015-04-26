3543c4237dd5c41aaa031ced17ef01db0b051e33
zyfcs3 <zhuyafeng911@163.com>
2015-04-26 19:13:33 +0800

    t

--- Test/src/main/java/com/taotao/manage/datasource/GitHubTest.java
+++ Test/src/main/java/com/taotao/manage/datasource/GitHubTest.java
@@ -4,7 +4,7 @@ public class GitHubTest {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("aaa");
+        System.out.println("fdfd");
     }
 
 }
